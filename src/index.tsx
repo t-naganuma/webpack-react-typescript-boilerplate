82bfdebadb605dbf7e6209dbeb0bd242cef883fe
t-naganuma <takuya.naganuma@gmail.com>
2022-03-05 23:23:22 +0900

    distをgitignore

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
-  return <div>Hello</div>
+  return (
+    <div>
+      Hello
+    </div>
+  )
 };
 
 ReactDOM.render(
