72d08038920118e874a169a96ed1640ddc741974
t-naganuma <takuya.naganuma@gmail.com>
2022-03-05 19:57:58 +0900

    babelでトランスパイル

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <div>Hello</div>
+};
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+)
--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,30 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/index.tsx",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.*(ts|tsx)$/,
+        use: ["babel-loader"],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, "src"), "node_modules"],
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: "output",
+      template: "src/index.html",
+      filename: "index.html",
     }),
   ],
+  devServer: {},
 };
