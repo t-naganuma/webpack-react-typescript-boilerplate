26b2ac563e0cf00b1432f849fab49aa22893da83
t-naganuma <takuya.naganuma@gmail.com>
2022-04-10 12:08:39 +0900

    eslint設定

---
+++ .eslintrc.js
@@ -0,0 +1,84 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "airbnb",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    project: "./tsconfig.eslint.json",
+    tsconfigRootdir: __dirname,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "import", "jsx-a11y", "react", "react-hooks"],
+  root: true,
+  rules: {
+    "lines-between-class-members": [
+      "error",
+      "always",
+      {
+        exceptAfterSingleLine: true,
+      },
+    ],
+    "no-void": [
+      "error",
+      {
+        allowAsStatement: true,
+      },
+    ],
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        vars: "all",
+        args: "after-used",
+        argsIgnorePattern: "_",
+        ignoreRestSiblings: false,
+        varsIgnorePattern: "_",
+      },
+    ],
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        js: "never",
+        jsx: "never",
+        ts: "never",
+        tsx: "never",
+      },
+    ],
+    "react/jsx-filename-extension": [
+      "error",
+      {
+        extensions: [".jsx", ".tsx"],
+      },
+    ],
+    "react/react-in-jsx-scope": "off",
+  },
+  overrides: [
+    {
+      files: ["*.tsx"],
+      rules: {
+        "react/prop-types": "off",
+      },
+    },
+  ],
+  settings: {
+    "import/resolver": {
+      node: {
+        paths: ["src"],
+      },
+    },
+  },
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,39 +1,40 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import styles from "./index.scss"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import styles from './index.scss';
 
-const Header = () => {
-  return <header className={styles.header}>Header</header>
+function Header() {
+  return <header className={styles.header}>Header</header>;
 }
 
-const Main = ({name}: {name: string}) => {
+function Main({ name }: {name: string}) {
   return (
     <main className={styles.main}>
       <section>
         <div>Main</div>
-        <p className={styles.name}>Hello, {name}</p>
+        <p className={styles.name}>
+          Hello,
+          {name}
+        </p>
       </section>
     </main>
-  )
+  );
 }
 
-const Footer = () => {
-  return <footer className={styles.footer}>footer</footer>
+function Footer() {
+  return <footer className={styles.footer}>footer</footer>;
 }
 
-const App = () => {
+function App() {
   return (
     <div className={styles.wrapper}>
       <Header />
       <Main name="webpack" />
       <Footer />
     </div>
-  )
-};
-
-
+  );
+}
 
 ReactDOM.render(
   <App />,
-  document.getElementById('app')
-)
+  document.getElementById('app'),
+);
